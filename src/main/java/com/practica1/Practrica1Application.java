bfd76ab42437782f547db76a9a7e6f769882a28a
Cami3020 <142687640+Cami3020@users.noreply.github.com>
2024-06-10 12:15:07 -0600

    Practica 2 o tarea 1

---
+++ src/main/java/com/practica1/Practrica1Application.java
@@ -0,0 +1,13 @@
+package com.practica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practrica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practrica1Application.class, args);
+	}
+
+}
